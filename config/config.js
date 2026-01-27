d504da496c8b37008e987e554b9817be369ceb2e
mau <mau.maun30@gmail.com>
2026-01-27 14:17:15 +0800

    env fix

--- config/config.js
+++ config/config.js
@@ -1,26 +1,29 @@
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
   },
+
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE + "_test",
+    database: `${process.env.DB_DATABASE}_test`,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
   },
+
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
